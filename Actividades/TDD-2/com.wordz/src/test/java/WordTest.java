d7c233ce8a37e80a875a650487736e00509f9620
llVitell <83891398+llVitell@users.noreply.github.com>
2023-04-05 09:02:36 -0500

    Diseño Final

--- Actividades/TDD-2/com.wordz/src/test/java/WordTest.java
+++ Actividades/TDD-2/com.wordz/src/test/java/WordTest.java
@@ -1,11 +1,14 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class WordTest {
     @Test
     public void oneIncorrectLetter() {
         var word = new Word("A");
         var score = word.guess("Z");
         var result = score.letter(0);
-        assertThat(result).isEqualTo(Letter.INCORRECT);
+        // assertThat(result).isEqualTo(Letter.INCORRECT);
+        assertEquals(result,Letter.INCORRECT);
     }
 }
