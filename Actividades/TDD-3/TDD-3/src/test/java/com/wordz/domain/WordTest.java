9d9833c385ca32860b6bb707d606db4a217a96c8
Jhonnatan E. Rojas <62492146+Jhonnatan1806@users.noreply.github.com>
2023-04-12 08:36:44 -0500

    refactorizacion

--- Actividades/TDD-3/TDD-3/src/test/java/com/wordz/domain/WordTest.java
+++ Actividades/TDD-3/TDD-3/src/test/java/com/wordz/domain/WordTest.java
@@ -10,8 +10,12 @@ public class WordTest {
     public void oneCorrectLetter() {
         var word = new Word("A");
         var score = word.guess("A");
-        assertThat(score.letter(0))
-                .isEqualTo(Letter.CORRECT);
+        assertScoreForLetter(score, 0, Letter.CORRECT);
+    }
+    private void assertScoreForLetter(Score score,
+                                      int position, Letter expected) {
+        assertThat(score.letter(position))
+                .isEqualTo(expected);
     }
 
 }
