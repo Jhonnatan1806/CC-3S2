90bc8d6c418d212372d26fab697737104b2ee29b
llVitell <omar.vite.a@uni.pe>
2023-04-14 21:11:38 -0500

    DivisonTest

--- Proyecto-3S2/Spring.0/Pruebas Unitarias/app/test/division.test.ts
+++ Proyecto-3S2/Spring.0/Pruebas Unitarias/app/test/division.test.ts
@@ -3,5 +3,5 @@ import { division } from '../src/division';
 test('Prueba de division', () => {
   expect(division(6, 3)).toBe(2); // Se espera que la division de 6 y 3 sea igual a 2
   expect(division(0, 2)).toBe(0); // Se espera que la division de 0 y 2 sea igual a 0
-  expect(division(0, 0)).toThrow("No se puede dividir entre cero"); // Se espera que la division sea indefinida
+  expect(() => division(0, 0)).toThrow("No se puede dividir entre cero"); // Se espera que la division sea indefinida
 });
