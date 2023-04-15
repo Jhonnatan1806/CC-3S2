af29ab3b3f71bb7cbf7e9f193b668aef99a1e1c0
llVitell <omar.vite.a@uni.pe>
2023-04-15 00:44:10 -0500

    GUI - 1

--- Proyecto-3S2/Spring.0/Programacion GUI/sos/src/pages/index.tsx
+++ Proyecto-3S2/Spring.0/Programacion GUI/sos/src/pages/index.tsx
@@ -1,7 +1,45 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Hola mundo</h1>
+    <main className="h-screen">
+      <section className="h-full w-full flex justify-center items-center">
+        <div className="flex flex-col w-[750px] h-[600px] bg-blue-200 p-8 rounded-xl">
+          <div className="flex justify-between">
+            <div>SOS</div>
+            <div>Single Game</div>
+            <div>General Game</div>
+            <div>Board Size</div>
+          </div>
+          <div className="flex flex-grow mt-8">
+            <div className="flex flex-col justify-between w-[15%]">
+              <div>Blue Player</div>
+              <div>
+                <div>Human</div>
+                <div>Computer</div>
+              </div>
+              <div>Record Game</div>
+            </div>
+            <div className="flex flex-col flex-grow mx-8">
+              <div className="flex flex-grow bg-white rounded-xl">
+                .
+              </div>
+              <div className="flex justify-center mt-8">
+                Current Turn
+              </div>
+            </div>
+            <div className="flex flex-col justify-between w-[15%] text-right">
+              <div>Red Player</div>
+              <div>
+                <div>Human</div>
+                <div>Computer</div>
+              </div>
+              <div>
+                <div>Replay</div>
+                <div>New Game</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
