bd02198d349a1dbc95b50464052e5a7f64122d28
Jhonnatan E. Rojas <62492146+Jhonnatan1806@users.noreply.github.com>
2023-04-16 13:52:31 -0500

    se agrego los radiobuttons de blueplayer y redplayer

--- Proyecto-3S2/Sprint.0/Programacion GUI/sos/src/pages/index.tsx
+++ Proyecto-3S2/Sprint.0/Programacion GUI/sos/src/pages/index.tsx
@@ -4,6 +4,7 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -12,9 +13,12 @@ export default function Home() {
         <div className="flex flex-col w-[750px] h-[600px] bg-gray-300 p-8 rounded-xl">
           <div className="flex justify-between items-center">
             <div className="w-[15%] flex justify-center">
-              <img
+              <Image
                 src="https://cdn2.iconfinder.com/data/icons/symbols-8/50/1F198-sos-button-A-512.png"
+                alt="SOS button"
                 className="max-w-[60px]"
+                width={60}
+                height={60}
               />
             </div>
             <FormControl>
@@ -37,29 +41,95 @@ export default function Home() {
             </FormControl>
             <div className="w-[15%] flex justify-center">Board Size</div>
           </div>
-          <div className="flex flex-grow mt-8">
+          <div className="flex flex-grow mt-8 gap-4 ">
             <div className="flex flex-col justify-between w-[15%]">
-              <div>Blue Player</div>
-              <div>
-                <div>Human</div>
-                <div>Computer</div>
-              </div>
-              <div>Record Game</div>
+              <FormControl>
+                <FormLabel>Blue Player</FormLabel>
+                <RadioGroup
+                  row
+                  aria-labelledby="demo-row-radio-buttons-group-label"
+                  name="row-radio-buttons-group"
+                >
+                  <FormControlLabel
+                    value="human"
+                    control={<Radio />}
+                    label="Human"
+                  />
+                    <RadioGroup
+                      row
+                      aria-labelledby="demo-row-radio-buttons-group-label"
+                      name="row-radio-buttons-group"
+                    >
+                      <FormControlLabel
+                        value="S"
+                        control={<Radio />}
+                        label="S"
+                        style={{ marginLeft: '10px' }}
+                      />
+                      <FormControlLabel
+                        value="O"
+                        control={<Radio />}
+                        label="O"
+                        style={{ marginLeft: '10px' }}
+                      />
+                    </RadioGroup>
+                  <FormControlLabel
+                    value="computer"
+                    control={<Radio />}
+                    label="Computer"
+                  />
+                </RadioGroup>
+              </FormControl>
             </div>
-            <div className="flex flex-col flex-grow mx-8">
-              <div className="flex flex-grow bg-white rounded-xl">.</div>
-              <div className="flex justify-center mt-8">Current Turn</div>
+            <div className="flex flex-grow h-full bg-white rounded-xl">
+              <canvas id="canvasGame"></canvas>
             </div>
-            <div className="flex flex-col justify-between w-[15%] text-right">
-              <div>Red Player</div>
-              <div>
-                <div>Human</div>
-                <div>Computer</div>
-              </div>
-              <div>
-                <div>Replay</div>
-                <div>New Game</div>
-              </div>
+            <div className="flex flex-col justify-between w-[15%]">
+              <FormControl>
+                <FormLabel>Red Player</FormLabel>
+                <RadioGroup
+                  row
+                  aria-labelledby="demo-row-radio-buttons-group-label"
+                  name="row-radio-buttons-group"
+                >
+                  <FormControlLabel
+                    value="human"
+                    control={<Radio />}
+                    label="Human"
+                  />
+                  <RadioGroup
+                      row
+                      aria-labelledby="demo-row-radio-buttons-group-label"
+                      name="row-radio-buttons-group"
+                    >
+                      <FormControlLabel
+                        value="S"
+                        control={<Radio />}
+                        label="S"
+                        style={{ marginLeft: '10px' }}
+                      />
+                      <FormControlLabel
+                        value="O"
+                        control={<Radio />}
+                        label="O"
+                        style={{ marginLeft: '10px' }}
+                      />
+                    </RadioGroup>
+                  <FormControlLabel
+                    value="computer"
+                    control={<Radio />}
+                    label="Computer"
+                  />
+                </RadioGroup>
+              </FormControl>
+            </div>
+          </div>
+          <div className="flex justify-between items-center flex-wrap mt-8 ">
+            <div>Record Game</div>
+            <div>Current Turn</div>
+            <div>
+              <div>Replay</div>
+              <div>New Game</div>
             </div>
           </div>
         </div>
