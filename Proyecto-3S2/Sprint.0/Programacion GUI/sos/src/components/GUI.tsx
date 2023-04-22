5a4d056c412a5eba3e71de81843dac76a734af55
Jhonnatan E. Rojas <62492146+Jhonnatan1806@users.noreply.github.com>
2023-04-22 14:03:13 -0500

    Se creo GUI y Tablero

---
+++ Proyecto-3S2/Sprint.0/Programacion GUI/sos/src/components/GUI.tsx
@@ -0,0 +1,97 @@
+import Image from "next/image"
+import { Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material"
+import Tablero from "./Tablero"
+
+type Player = {
+  name: string
+}
+
+function PlayerMovement( {name}: Player ){
+  return (
+    <div >
+      <FormControl>
+      <FormLabel>{name} Player</FormLabel>
+      <RadioGroup
+        aria-labelledby="radio-buttons-group-label"
+        name="radio-buttons-group"
+      >
+        <FormControlLabel
+          value="human"
+          control={<Radio />}
+          label="Human"
+        />
+          <RadioGroup
+            aria-labelledby="radio-buttons-group-label"
+            name="radio-buttons-group"
+          >
+            <FormControlLabel
+              value="S"
+              control={<Radio />}
+              label="S"
+              style={{ marginLeft: '10px' }}
+            />
+            <FormControlLabel
+              value="O"
+              control={<Radio />}
+              label="O"
+              style={{ marginLeft: '10px' }}
+            />
+          </RadioGroup>
+        <FormControlLabel
+          value="computer"
+          control={<Radio />}
+          label="Computer"
+        />
+      </RadioGroup>
+    </FormControl>
+  </div>
+  )
+}
+
+export default function GUI() {
+  return (
+    <>
+      <main className="flex flex-col max-w-4xl w-full h-fit rounded-xl gap-4 p-4 m-auto bg-gray-200 ">
+        <div className="flex flex-row flex-wrap justify-between items-center">
+          <Image src='/images/logo.webp' alt='Logo' width={64} height={64} />
+          <FormControl>
+            <RadioGroup
+              row
+              aria-labelledby="row-radio-buttons-group-label"
+              name="row-radio-buttons-group"
+            >
+            <FormControlLabel
+                value="simple"
+                control={<Radio />}
+                label="Simple Game"
+            />
+            <FormControlLabel
+              value="general"
+              control={<Radio />}
+              label="General Game"
+            />
+            </RadioGroup>
+          </FormControl>
+          <p><strong>Board Size: </strong>3x3</p>
+        </div>
+        <div className="flex flex-col items-center md:items-stretch md:flex-row gap-4">
+          <div className="flex flex-col justify-between w-1/2 md:w-[18%]">
+            <PlayerMovement name="Blue" />
+            <Button variant="contained">Record Game</Button>
+          </div>
+          <div className="flex flex-col flex-grow gap-4 items-center">
+            <Tablero/>
+            <p><strong>Current Turn: </strong>blue (or red)</p>
+          </div>
+          <div className="flex flex-col justify-between w-1/2 md:w-[18%]">
+            <PlayerMovement name="Red" />
+            <div className="flex flex-col gap-2">
+              <Button variant="contained">Replay</Button>
+              <Button variant="contained">New Game</Button>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>    
+  )
+}
---
+++ Proyecto-3S2/Sprint.0/Programacion GUI/sos/src/components/Tablero.tsx
@@ -0,0 +1,8 @@
+export default function Tablero() {
+  return (
+    <>
+      <div className="min-w-[400px] w-full h-[500px] rounded-xl bg-white">
+      </div>  
+    </>    
+  )
+}
