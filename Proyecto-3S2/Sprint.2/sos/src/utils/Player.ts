e92577f51eb897e8d1b82b35973da9ffc0c4ffb7
Jhonnatan E. Rojas <62492146+Jhonnatan1806@users.noreply.github.com>
2023-04-23 16:46:15 -0500

    Se creo la clase Player

---
+++ Proyecto-3S2/Sprint.2/sos/src/utils/Player.ts
@@ -0,0 +1,44 @@
+import Board from './Board';
+
+/**
+ * Representa un jugador del juego SOS.
+ * @author Jhonnnatan && Omar
+ */
+export default class Player {
+
+  private readonly name: string;
+  private readonly board: Board;
+
+  /**
+   * Crea un nuevo jugador con el nombre especificado.
+   * 
+   * @param name El nombre del jugador.
+   * @param board El tablero del juego.
+   */
+  constructor (name: string, board: Board) {
+    this.name = name;
+    this.board = board;
+  }
+
+  /**
+   * Devuelve el nombre del jugador.
+   * 
+   * @returns El nombre del jugador.
+   */
+  public getName(): string {
+    return this.name;
+  }
+
+  /**
+   * Selecciona el modo de juego del tablero.
+   * 
+   * @param mode El modo de juego del tablero.
+   * @returns El modo de juego del tablero.
+   * @see Board.setGameMode
+   */
+  public selectGameMode(mode: boolean): boolean {
+    this.board.setGameMode(mode);
+    return this.board.getGameMode();
+  }
+
+}
